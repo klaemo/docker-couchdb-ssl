75a7014519b7375639a480c1ed836a3cdc368abd
klaemo <clemens.stolle@gmail.com>
2014-08-27 01:07:11 +0200

    set permissions on run

--- entrypoint-stud.sh
+++ entrypoint-stud.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+set -e;
+
+chown _stud: /usr/local/etc/stud/stud.pem
+chown _stud: /var/run/stud
+chown -R _stud: /usr/local/var/run/stud /usr/local/etc/stud
+chmod 0770 /usr/local/var/run/stud
 
 chmod 664 /usr/local/etc/stud/*.conf
 chmod 600 /usr/local/etc/stud/stud.pem
